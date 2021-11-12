47758b9a4a41035380803a49b7751a1fb6e779a5
lee-vg <lee.brooks@vatglobal.com>
2021-11-12 21:38:29 +0200

    Add extra test case

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,6 +6,7 @@ from forecast.utils import calculate_stats
 @pytest.mark.parametrize(
     ("forecast", "expected"),[
         ([], {}),
+        ([20], {'average': 20, 'maximum': 20, 'median': 20, 'minimum': 20}),
         ([20, 10.5, 15, 9], {'average': 13.625, 'maximum': 20, 'median': 12.75, 'minimum': 9})
     ])
 def test_calculate_stats(forecast, expected):
